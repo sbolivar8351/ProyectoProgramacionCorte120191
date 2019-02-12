9fddd932258832ed216b077bcffc08696fa44755
Jose Cordoba <jmcordoba@universidadean.edu.co>
2019-02-12 13:49:49 -0500

    Add codigo fuente de pruebas.py

---
+++ pruebas.py
@@ -0,0 +1,11 @@
+import unittest
+import funciones as f
+
+class pruebas(unittest.TestCase):
+
+    def test_calcular_precio_producto(self):
+        self.assertEqual(f.calcular_precio_producto(1000), 1500)
+        self.assertEqual(f.calcular_precio_producto(0), 0)
+
+if __name__ == 'main':
+    unittest.main()
