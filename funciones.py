80fbff4b54b371ca1895e92ef2c5717ce6f1a2b8
Sebastian Bolivar <sbolivar8351@universidadean.edu.co>
2019-02-26 17:36:18 -0500

    desarrollo de las funciones con sus pruebas

--- funciones.py
+++ funciones.py
@@ -1,53 +1,55 @@
+
 def calcular_precio_producto(coste_producto):
-    pass
+    return coste_producto + (coste_producto*0.5)
 
 
 def calcular_precio_servicio(cantidad_horas):
-    pass
+    return cantidad_horas * 100000
 
 
 def calcular_precio_servicio_extras(cantidad_horas):
-    pass
+    tarifa = calcular_precio_servicio(cantidad_horas)
+    return tarifa + (tarifa*0.25)
 
 
 def calcular_costo_envio(kilometros):
-    pass
+    return kilometros *115
 
 def calcular_precio_producto_fuera(coste_producto,
                                    kilometros):
-    pass
+    return calcular_precio_producto(coste_producto) +calcular_costo_envio(kilometros)
 
 
 def calcular_iva_producto(coste_producto, tasa):
-    pass
+    return calcular_precio_producto(coste_producto) * (tasa/100)
 
 
 def calcular_iva_servicio(cantidad_horas, tasa):
-    pass
+    return calcular_precio_servicio(cantidad_horas) * (tasa/100)
 
 
 def calcular_iva_envio(kilometros, tasa):
-    pass
+    return calcular_costo_envio(kilometros) * (tasa/100)
 
 
-def calcular_iva_servicio_fuera(cantidad_horas, tasa):
-    pass
+def calcular_iva_servicio_extra(cantidad_horas, tasa):
+    return calcular_precio_servicio_extras(cantidad_horas)  * (tasa/100)
 
 
 def calcular_recaudo_locales(coste_producto_1,
                              coste_producto_2,
                              coste_producto_3):
-    pass
+    return calcular_precio_producto(coste_producto_1) + calcular_precio_producto(coste_producto_2) + calcular_precio_producto(coste_producto_3)
 
 def calcular_recaudo_horas_extra(horas_1,
                                  horas_2,
                                  horas_3,
                                  horas_4):
-    pass
+    return calcular_precio_servicio_extras(horas_1) + calcular_precio_servicio_extras(horas_2) + calcular_precio_servicio_extras(horas_3) + calcular_precio_servicio_extras(horas_4)
 
 
 def calcular_recaudo_mixto_local(coste_producto_1,
                                  coste_producto_2,
                                  horas_1,
                                  horas_2):
-    pass
+    return calcular_precio_producto(coste_producto_1) + calcular_precio_producto(coste_producto_2) + calcular_precio_servicio_extras(horas_1) + calcular_precio_servicio_extras(horas_2)
--- pruebas.py
+++ pruebas.py
@@ -8,37 +8,43 @@ class pruebas(unittest.TestCase):
         self.assertEqual(f.calcular_precio_producto(0), 0)
 
     def test_calcular_precio_servicio(self):
-        pass
+        self.assertEqual(f.calcular_precio_servicio(2), 200000)
+        self.assertEqual(f.calcular_precio_servicio(0), 0)
 
     def test_calcular_precio_servicio_extras(self):
-        pass
+        self.assertEqual(f.calcular_precio_servicio_extras(2), 250000)
+        self.assertEqual(f.calcular_precio_servicio_extras(0), 0)
 
     def test_calcular_costo_envio(self):
-        pass
+        self.assertEqual(f.calcular_costo_envio(1000), 115000)
+        self.assertEqual(f.calcular_costo_envio(0), 0)
 
     def test_calcular_precio_producto_fuera(self):
-        pass
+        self.assertEqual(f.calcular_precio_producto_fuera(1000,2), 1730)
+        self.assertEqual(f.calcular_precio_producto_fuera(0,0), 0)
 
     def test_calcular_iva_producto(self):
-        pass
+        self.assertEqual(f.calcular_iva_producto(1000,19.0), 285)
 
     def test_calcular_iva_servicio(self):
-        pass
+        self.assertEqual(f.calcular_iva_servicio(1,19.0), 19000)
 
     def test_calcular_iva_envio(self):
-        pass
+        self.assertEqual(f.calcular_iva_envio(1, 19.0), 21.85)
 
     def test_calcular_iva_servicio_fuera(self):
-        pass
+        self.assertEqual(f.calcular_iva_servicio_extra(1, 19.0), 23750)
+
 
     def test_calcular_recaudo_locales(self):
-        pass
+        self.assertEqual(f.calcular_recaudo_locales(100,200,300),900)
+
 
     def test_calcular_recaudo_horas_extra(self):
-        pass
+        self.assertEqual(f.calcular_recaudo_horas_extra(1,2,3,4),1250000)
 
     def test_calcular_recaudo_mixto_local(self):
-        pass
+        self.assertEqual(f.calcular_recaudo_mixto_local(100,200,1,2),375450)
 
 
 if __name__ == 'main':
