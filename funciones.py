7cec614a8d796a44b71770924daf48922ce1b6ad
Sebastian Bolivar <sbolivar8351@universidadean.edu.co>
2019-02-26 17:51:28 -0500

    Estructura de comentario en cada funcion

--- funciones.py
+++ funciones.py
@@ -1,55 +1,135 @@
 
+
+
 def calcular_precio_producto(coste_producto):
-    return coste_producto + (coste_producto*0.5)
+    """
+
+    :param coste_producto:
+    :return:
+    """
+    return coste_producto + (coste_producto * 0.5)
 
 
 def calcular_precio_servicio(cantidad_horas):
+    """
+
+    :param cantidad_horas:
+    :return:
+    """
     return cantidad_horas * 100000
 
 
 def calcular_precio_servicio_extras(cantidad_horas):
+    """
+
+    :param cantidad_horas:
+    :return:
+    """
     tarifa = calcular_precio_servicio(cantidad_horas)
-    return tarifa + (tarifa*0.25)
+    return tarifa + (tarifa * 0.25)
 
 
 def calcular_costo_envio(kilometros):
-    return kilometros *115
+    """
+
+    :param kilometros:
+    :return:
+    """
+    return kilometros * 115
+
 
 def calcular_precio_producto_fuera(coste_producto,
                                    kilometros):
-    return calcular_precio_producto(coste_producto) +calcular_costo_envio(kilometros)
+    """
+
+    :param coste_producto:
+    :param kilometros:
+    :return:
+    """
+    return calcular_precio_producto(coste_producto) + calcular_costo_envio(kilometros)
 
 
 def calcular_iva_producto(coste_producto, tasa):
-    return calcular_precio_producto(coste_producto) * (tasa/100)
+    """
+
+    :param coste_producto:
+    :param tasa:
+    :return:
+    """
+    return calcular_precio_producto(coste_producto) * (tasa / 100)
 
 
 def calcular_iva_servicio(cantidad_horas, tasa):
-    return calcular_precio_servicio(cantidad_horas) * (tasa/100)
+    """
+
+    :param cantidad_horas:
+    :param tasa:
+    :return:
+    """
+    return calcular_precio_servicio(cantidad_horas) * (tasa / 100)
 
 
 def calcular_iva_envio(kilometros, tasa):
-    return calcular_costo_envio(kilometros) * (tasa/100)
+    """
+
+    :param kilometros:
+    :param tasa:
+    :return:
+    """
+    return calcular_costo_envio(kilometros) * (tasa / 100)
 
 
 def calcular_iva_servicio_extra(cantidad_horas, tasa):
-    return calcular_precio_servicio_extras(cantidad_horas)  * (tasa/100)
+    """
+
+    :param cantidad_horas:
+    :param tasa:
+    :return:
+    """
+    return calcular_precio_servicio_extras(cantidad_horas) * (tasa / 100)
 
 
 def calcular_recaudo_locales(coste_producto_1,
                              coste_producto_2,
                              coste_producto_3):
-    return calcular_precio_producto(coste_producto_1) + calcular_precio_producto(coste_producto_2) + calcular_precio_producto(coste_producto_3)
+    """
+
+    :param coste_producto_1:
+    :param coste_producto_2:
+    :param coste_producto_3:
+    :return:
+    """
+    return calcular_precio_producto(coste_producto_1) + calcular_precio_producto(
+        coste_producto_2) + calcular_precio_producto(coste_producto_3)
+
 
 def calcular_recaudo_horas_extra(horas_1,
                                  horas_2,
                                  horas_3,
                                  horas_4):
-    return calcular_precio_servicio_extras(horas_1) + calcular_precio_servicio_extras(horas_2) + calcular_precio_servicio_extras(horas_3) + calcular_precio_servicio_extras(horas_4)
+    """
+
+    :param horas_1:
+    :param horas_2:
+    :param horas_3:
+    :param horas_4:
+    :return:
+    """
+    return calcular_precio_servicio_extras(horas_1) + calcular_precio_servicio_extras(
+        horas_2) + calcular_precio_servicio_extras(horas_3) + calcular_precio_servicio_extras(horas_4)
 
 
 def calcular_recaudo_mixto_local(coste_producto_1,
                                  coste_producto_2,
                                  horas_1,
                                  horas_2):
-    return calcular_precio_producto(coste_producto_1) + calcular_precio_producto(coste_producto_2) + calcular_precio_servicio_extras(horas_1) + calcular_precio_servicio_extras(horas_2)
+    """
+
+    :param coste_producto_1:
+    :param coste_producto_2:
+    :param horas_1:
+    :param horas_2:
+    :return:
+    """
+    return calcular_precio_producto(coste_producto_1) + calcular_precio_producto(
+        coste_producto_2) + calcular_precio_servicio_extras(horas_1) + calcular_precio_servicio_extras(horas_2)
